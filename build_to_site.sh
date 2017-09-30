25073c69c9684e169bbcb266ac7d22ab1eb42785
speedio <njnu_wy@126.com>
2017-09-30 11:24:50 +0800

    update at 2021-07-20

---
+++ build_to_site.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bundle exec jekyll build -d ../neo-hpstr-jekyll-wi_site
---
+++ only_drafts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bundle exec jekyll serve --watch --config _config.yml,_config_drafts.yml --drafts
